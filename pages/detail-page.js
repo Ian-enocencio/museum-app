16ce15e38ca9e146e1546e75e3de3d574c6ffb87
Ian-enocencio <i.e.vandervlist@gmail.com>
2019-07-03 17:38:19 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,10 +1,28 @@
+function doesNotPassAllValidations (name, msg) {
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message')
+        return true;
+    }
+
+    if(msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }
+
+    return false
+}
 function submitComment() {
 // ---- gather data
    const inputField = document.getElementById('name')
    const name = inputField.value
    const textArea = document.getElementById('msg')
    const msg = textArea.value
+  
+   if(doesNotPassAllValidations(name, msg)){
+    return null
+  }
 
+  
    //---- creating elements
    const comment = document.createElement('section') // creating section with .createElement
    const h3 = document.createElement('h3')
@@ -25,5 +43,5 @@ function submitComment() {
    //-- reset value
    inputField.value = null
    textArea.value = null
-   
+   console.log(name)
 }
