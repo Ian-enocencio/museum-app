86b65d27919eb36006296f3bd3a8ac93824fc4cd
Ian-enocencio <i.e.vandervlist@gmail.com>
2019-07-03 16:27:43 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,20 @@
 function submitComment() {
-    console.log('hello')
+// ---- gather data
+   const inputField = document.getElementById('name')
+   const name = inputField.value
+   const textArea = document.getElementById('msg')
+   const msg = textArea.value
+
+   //---- creating elements
+   const comment = document.createElement('section') // creating section with .createElement
+   const h3 = document.createElement('h3')
+   const p = document.createElement('p')
+
+   // --- adjusting the elements
+   h3.innerHTML = `${name} said:`
+   p.innerHTML = msg
+   comment.classList.add('comment')
+   comment.appendChild(h3)
+   comment.appendChild(p)
+   console.log(comment)
 }
